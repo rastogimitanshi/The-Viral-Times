cbaf3d8ceaa41f46f54bbc5d2af3fe004ac0db3d
Mitanshi Rastogi <mitanshi1995.mr@gmail.com>
2017-06-10 23:46:10 +0530

    Uplaod PPT

---
+++ watch.py
@@ -0,0 +1,17 @@
+from Tkinter import *
+import time
+root= Tk()
+time1=''
+clock= Label(root, font=('times',20,'bold'), bg='white')
+clock.pack(fill=BOTH, expand=1)
+
+def tick():
+    global time1
+    time2= time.strftime('%H:%M:%S')
+    if time2!=time1:
+        time1=time2
+        clock.config(text=time2)
+    clock.after(200, tick)
+
+tick()
+root.mainloop()
